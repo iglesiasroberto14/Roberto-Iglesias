eaf591d6b5c45ae003e6984c9198f9a4a59516dd
iglesiasroberto14 <iglesiasroberto14@gmail.com>
2015-04-16 11:03:48 -0300

    TP #1 Cargado
    Programa que realiza las operaciones mas importantes para los distintos
    tipos de datos

---
+++ Tipos de datos.cpp
@@ -0,0 +1,89 @@
+/*
+Roberto Iglesias
+14042015
+Programa demostrativo de tipos de datos y operaciones
+*/
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main (){
+	
+	int entero1=0, entero2=0;
+	float real1=0, real2=0;
+	char pal1[64], pal2[64], oracion[128];
+	bool booleano1, booleano2; 
+	
+	/*Ingreso de los numeros enteros*/
+	cout << "Ingrese un numero entero A" <<endl;
+	cin >> entero1;
+	cout << "Ingrese un numero entero B" <<endl;
+	cin >> entero2;
+	
+	/*Operaciones con los numeros enteros*/
+	cout << "Suma\n" << entero1+entero2 << endl << endl; 
+	cout << "Resta\n" << entero1-entero2 << endl << endl; 
+	cout << "Multiplicacion\n" << entero1*entero2 << endl << endl; 
+	cout << "Division\n" << entero1/entero2 << endl << endl; 
+	cout << "Resto de la division entera\n" << entero1%entero2 << endl << endl; 
+	/*Comparacion entre los numeros enteros*/
+	if (entero1==entero2) {
+	cout<<"Los numeros son iguales"<<endl << endl;
+	}
+	 else{
+		if(entero1>entero2) cout<<"El numero A es mayor" << endl << endl;
+		else cout<<"El numero B es mayor"<<endl << endl;
+	}
+	
+	/*Ingreso de los numeros con coma flotante*/
+	cout << "Ingrese un numero real A" <<endl;
+	cin >> real1;
+	cout << "Ingrese un numero real B" <<endl;
+	cin >> real2;
+	
+	/*Operaciones con los numeros con coma flotante*/
+	cout << "Suma\n" << real1+real2 << endl << endl; 
+	cout << "Resta\n" << real1-real2 << endl << endl; 
+	cout << "Multiplicacion\n" << real1*real2 << endl << endl; 
+	cout << "Division\n" << real1/real2 << endl << endl; 
+	
+	/*Comparacion entre los numeros con coma flotante*/
+		if (real1==real2) {
+	cout<<"Los numeros son iguales"<<endl;
+	}
+	 else{
+		if(real1<real2) cout<<"El numero A es menor" << endl;
+		else cout<<"El numero B es menor"<<endl;
+	}
+	
+	/*Ingreso de los datos alfanumericos*/
+	cout << "Ingrese un texto 'A'" << endl;
+	cin >> pal1;
+	cout << "Ingrese un texto 'B'" <<endl;
+	cin >> pal2;
+	
+	/*Operaciones con caracteres*/
+	strcat(oracion, pal1);
+	strcat(oracion, pal2);
+	cout << oracion << endl;
+	*oracion = *pal1 & *pal2;
+	cout << oracion << endl;
+	
+	
+	/*Ingreso de variables booleanas*/
+	cout << "Ingrese 1 o 0 para booleano A" << endl;
+	cin >> booleano1;
+	cout << "Ingrese 1 o 0 para booleano B" <<endl;
+	cin >> booleano2;
+	
+	if (booleano1==booleano2)	cout << "Ambos booleanos son iguales"<< endl;
+	else {
+		if (booleano1>booleano2) cout << "Booleano A es 1 y booleano B es 0" << endl;
+		else cout << "Booleano A es 0 y booleano B es 1" << endl;
+	}
+	
+	
+	return 0;
+}
+
